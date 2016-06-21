f1bbcf422596dbd7ac9f50600babbdd0432f7b7e
CoderXC <xiaobing_mailbox@163.com>
2016-06-21 15:41:10 +0800

    加入宏定义引用文件，方便直接使用

---
+++ XCImagePickerController/XCImagePickerController/AllDefiine.h
@@ -0,0 +1,13 @@
+//
+//  AllDefiine.h
+//  XCImagePickerController
+//
+//  Created by 小蔡 on 16/6/21.
+//  Copyright © 2016年 小蔡. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AllDefiine : NSObject
+
+@end
